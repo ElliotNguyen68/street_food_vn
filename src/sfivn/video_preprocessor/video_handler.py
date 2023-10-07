1014a6b0b884805f58dcd8993d400e032974d816
tanlocnguyen <elliotnguyen68@gmail.com>
2023-10-07 11:35:10 +0700

    video extract base video id

--- src/sfivn/video_preprocessor/video_handler.py
+++ src/sfivn/video_preprocessor/video_handler.py
@@ -151,7 +151,7 @@ def video_extract_base_on_id(
     list_features=[]
     list_frames=[]
     for frame_no in range(num_frames_in_video):
-        image_path = base_dir + "/{}/frames_{}".format(video_id,frame_no,)
+        image_path = base_dir + "/{}/frames_{}.jpg".format(video_id,frame_no,)
         
         value = extract_function(image_path)
         list_features.append(value)
