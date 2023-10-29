aeb60127e498602c061b86d595950c0905c2d086
tanlocnguyen <elliotnguyen68@gmail.com>
2023-10-29 09:32:47 +0700

    add extract full 150

--- src/sfivn/feature_extraction/scenes150/__init__.py
+++ src/sfivn/feature_extraction/scenes150/__init__.py
@@ -88,4 +88,60 @@ def extract_feature(
     return {
         key: value for key,value in get_list_tuple
     }
+
+
+def extract_feature_full(
+    segmentation_module: SegmentationModule, image_path: str, cuda: bool = False
+):
+    segmentation_module.eval()
+    if cuda:
+        segmentation_module.to(torch.device("cuda:0"))
+    else:
+        segmentation_module.cpu()
+
+    # Load and normalize one image as a singleton tensor batch
+    pil_to_tensor = torchvision.transforms.Compose(
+        [
+            torchvision.transforms.ToTensor(),
+            torchvision.transforms.Normalize(
+                mean=[0.485, 0.456, 0.406],  # These are RGB mean+std values
+                std=[0.229, 0.224, 0.225],
+            ),  # across a large photo dataset.
+        ]
+    )
+
+    pil_image = PIL.Image.open(image_path).convert("RGB")
+    # img_original = np.array(pil_image)
+    img_data = pil_to_tensor(pil_image)
+    if cuda:
+        singleton_batch = {"img_data": img_data[None].cuda()}
+    else:
+        singleton_batch = {"img_data": img_data[None]}
+    output_size = img_data.shape[1:]
+
+    # Run the segmentation at the highest resolution.
+    with torch.no_grad():
+        # this line take time
+        scores = segmentation_module(singleton_batch, segSize=output_size)
+
+    _, pred = torch.max(scores, dim=1)
+
+    unique_values, counts = np.unique(pred, return_counts=True)
+    counts = counts.astype("float64")
+    num_pixel = pred.shape[0] * pred.shape[1] * pred.shape[2]
+    logger.info(pred.shape)
+    # print(counts.sum())
+    # print(num_pixel)
+    counts /= num_pixel
+
+    zip_percentage_segment = [(key, value) for key, value in zip(unique_values, counts)]
+
+    zip_percentage_segment_sorted = sorted(
+        zip_percentage_segment, key=lambda x: x[1], reverse=True
+    )
     
+    get_list_tuple= zip_percentage_segment_sorted[:]
+    return {
+        key: value for key,value in get_list_tuple
+    }
+
